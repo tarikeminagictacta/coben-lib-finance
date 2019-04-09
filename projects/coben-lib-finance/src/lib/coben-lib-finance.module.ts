d10ee79c26b5993d111c374d99311c10661d3920
Tarik Eminagic <tarik.eminagic@cegeka.com>
2019-04-09 18:16:35 +0200

    feat(translations): added translaton to try lazy loaded module

--- projects/coben-lib-finance/src/lib/coben-lib-finance.module.ts
+++ projects/coben-lib-finance/src/lib/coben-lib-finance.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CobenLibCommonModule } from '@coben-lib/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { CobenLibFinanceComponent } from './coben-lib-finance.component';
 import { DetailsComponent } from './details/details.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CobenLibFinanceComponent, DetailsComponent],
   imports: [
     CobenLibCommonModule,
+    TranslateModule.forChild(),
     RouterModule
   ],
   exports: [CobenLibFinanceComponent, DetailsComponent]
