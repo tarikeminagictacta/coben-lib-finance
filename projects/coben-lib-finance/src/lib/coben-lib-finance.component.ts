e1bbf8a5c134486f3c6da3a228e29b76b0de291f
Tarik Eminagic <tarik.eminagic@cegeka.com>
2019-05-22 12:31:05 +0200

    feat(translations): try adding translations on module level

--- projects/coben-lib-finance/src/lib/coben-lib-finance.component.ts
+++ projects/coben-lib-finance/src/lib/coben-lib-finance.component.ts
@@ -1,17 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { TRANSLATIONS } from './finance-translations';
 
 @Component({
   selector: 'cblibfs-coben-lib-finance',
   templateUrl: './coben-lib-finance.component.html',
-  styleUrls: [ './coben-lib-finance.component.scss' ]
+  styleUrls: ['./coben-lib-finance.component.scss']
 })
 export class CobenLibFinanceComponent implements OnInit {
-  constructor(private translate: TranslateService) {
-    translate.setTranslation('en', TRANSLATIONS.en, true);
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
--- projects/coben-lib-finance/src/lib/coben-lib-finance.module.ts
+++ projects/coben-lib-finance/src/lib/coben-lib-finance.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CobenLibCommonModule } from '@coben-lib/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { CobenLibFinanceComponent } from './coben-lib-finance.component';
 import { DetailsComponent } from './details/details.component';
-
+import { TRANSLATIONS } from './finance-translations';
 
 @NgModule({
   declarations: [CobenLibFinanceComponent, DetailsComponent],
-  imports: [
-    CobenLibCommonModule,
-    TranslateModule,
-    RouterModule
-  ],
+  imports: [CobenLibCommonModule, TranslateModule, RouterModule],
   exports: [CobenLibFinanceComponent, DetailsComponent]
 })
-export class CobenLibFinanceModule { }
+export class CobenLibFinanceModule {
+  constructor(private translateService: TranslateService) {
+    console.log('Finance module ctor', translateService.instant('super.secret'));
+    translateService.setTranslation('en', TRANSLATIONS.en, true);
+    console.log('Finance module ctor', translateService.instant('super.secret'));
+  }
+}
